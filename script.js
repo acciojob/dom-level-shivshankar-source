7dce7ef77d72f4167837b1a36823aa93825140ec
shivshankar-source <shivshakti.ss771@gmail.com>
2025-11-10 13:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let ul=document.querySelector("ul")
 let children=ul.children
 let count=0
 for(let t of children){
-			count++;
+		
 	if(t.id=="level"){
 
 		prompt(`The level of the element is: ${count}`)
-		return;
+			
 	}
+	count++
 }
