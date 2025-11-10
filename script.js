a52ed993244aa8288a0f10b2f354c7d038cad502
shivshankar-source <shivshakti.ss771@gmail.com>
2025-11-10 13:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ul=document.querySelector("ul")
+let children=children.ul
+for(let t of children){
+	if(t.id==level){
+		prompt(`The level of the element is: ${t.id}`)
+		return;
+	}
+}
